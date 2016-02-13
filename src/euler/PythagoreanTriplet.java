e297f3d75f0337943f49ae6c7697ae323eac6bf6
seanBucholtz <sean.bucholtz@streambox.com>
2016-02-12 16:31:48 -0800

    LargestProductOfASequence JavaDocs created

---
+++ src/euler/PythagoreanTriplet.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package euler;
+
+/**
+ * @author seanbucholtz
+ *
+ */
+public class PythagoreanTriplet {
+
+}
---
+++ src/eulerTest/PythagoreanTripletTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package eulerTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author seanbucholtz
+ *
+ */
+public class PythagoreanTripletTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
